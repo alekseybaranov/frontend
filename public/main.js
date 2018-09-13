32cf96c10271c4aefb1935fa0e68a027deef30c3
Aleksey Baranov <albaranov@live.ru>
2018-09-13 20:05:07 +0300

    Урок 2-3

---
+++ public/main.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const sections = [
+  ['login', 'Окно регистрации'],
+  ['profile', 'Мой профиль'],
+  ['about', 'Об игре'],
+];
+
+const application = document.querySelector('#application');
+const nav = document.getElementById('navigation');
+
+for (let section of sections) {
+  const button = document.createElement('input');
+  button.setAttribute('type', 'button');
+  button.setAttribute('data-section', section[0]);
+  button.value = section[1];
+  nav.appendChild(button);
+}
+
+const liveSectionsCollection = application.getElementsByTagName('section');
+
+nav.addEventListener('click', function (event) {
+  const sectionId = event.target.getAttribute('data-section');
+  console.log(sectionId);
+
+  //console.log(liveSectionsCollection);
+
+  const liveSectionsArray = Array.from(liveSectionsCollection);
+
+
+  liveSectionsArray.forEach(function (sectionElement) {
+    sectionElement.hidden = true;
+    if (sectionElement.id === sectionId) {
+      sectionElement.hidden = false;
+    }
+  });
+  
+  
+});
+
+
+
+const greeting = document.getElementsByClassName('greeting')[0];
+
+greeting.addEventListener('click', () => {
+  greeting.classList.add('hidden');
+}, false);
--- server/server.js
+++ server/server.js
@@ -5,9 +5,12 @@ const fs = require('fs');
 
 const server = http.createServer(function(request, response) {
   console.log(request.method, request.url);
-  if (request.url == '/style.css') {
-    const css = fs.readFileSync('./public/style.css', 'utf8');
+  if (request.url == '/main.css') {
+    const css = fs.readFileSync('./public/main.css', 'utf8');
     response.end(css);
+  } else if(request.url == '/main.js') {
+    const text = fs.readFileSync('./public/main.js', 'utf8');
+    response.end(text);
   } else {
     const text = fs.readFileSync('./public/index.html', 'utf8');
     response.end(text);
