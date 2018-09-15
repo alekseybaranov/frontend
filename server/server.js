73bc09f98aa7215514b2600089923d9fb2e2eff2
Aleksey Baranov <albaranov@live.ru>
2018-09-15 13:22:40 +0300

    Express 04

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 // ----------------------------------------------------------------------------
 // Системные модули
+//
 
 const path = require('path');
 
 
 // ----------------------------------------------------------------------------
 // Сторонние модули
+//
 
 // модули сервера express и шаблонизатора handlebars
 const express = require(`express`),
@@ -17,6 +19,7 @@ const express = require(`express`),
 
 // ----------------------------------------------------------------------------
 // Собственные модули
+//
 
 
 
@@ -34,51 +37,64 @@ const express = require(`express`),
 
 // ----------------------------------------------------------------------------
 // Используемые каталоги
+//
 
 // каталог сервера
+//
 let serverDir = __dirname.replace(/\\/g, '/').toLowerCase();
 console.log('serverDir ==> ', serverDir);
 
 // каталог проекта
+//
 let baseDir = path.dirname(serverDir)
 console.log('baseDir ==> ', baseDir);
 
 // каталог внешних статических  файлов
+//
 //let publicDir = nconf.get(`publicDir`) || `/public`
 //publicDir = __dirname + publicDir
 let publicDir = baseDir + `/public`
 console.log('publicDir ==> ', publicDir);
 
 // каталог представлений
+//
 let viewsDir = serverDir + `/views`
 console.log('viewsDir ==> ', viewsDir);
 
 // каталог шаблонов
+//
 //let layoutsDir = nconf.get(`layoutsDir`) || `/views/layouts/`
 //layoutsDir = __dirname + layoutsDir
 let layoutsDir = serverDir + `/views/layouts`
 console.log('layoutsDir ==> ', layoutsDir);
 
 // каталог частичных шаблонов
+//
 //let partialsDir = nconf.get(`partialsDir`) || `/views/partials/`
 //partialsDir = __dirname + partialsDir
 let partialsDir = serverDir + `/views/partials`
 console.log('partialsDir ==> ', partialsDir);
 
 
-// порт сервера
+// порт http-сервера
+//
 //let port = nconf.get(`port`) // || 3000
-let port = process.env.PORT || 3000
+let port = process.env.PORT || 3000             // порт по умолчанию - 3000
 console.log('port ==>', port);
 
 
 // ----------------------------------------------------------------------------
-// Запускаем express
+// Настраиваем и запускаем express
 //
 const app = express();
-app.set(`port`, port)                           // порт HTTP-сервера
-app.set('views', viewsDir)                      // каталог представлений
 
+// Устанавливаем порт http-сервера
+//
+app.set(`port`, port)                           // порт HTTP-сервера
+
+// Подключаем шаблонизатор Handlebars
+//
+app.set('views', viewsDir)                      // каталог представлений
 app.engine(`hbs`, hbs( {
   extname: `hbs`,                               // расширение файлов-шаблонов
   defaultLayout: `main`,                        // основной шаблон
@@ -94,24 +110,27 @@ app.engine(`hbs`, hbs( {
 }))
 app.set('view engine', 'hbs')
 
-// Промежуточное ПО обработки статических файлов
+// Подключаем промежуточное ПО обработки статических файлов
+//
 app.use(express.static(publicDir));             // каталог статических файлов
 
-console.log(app);
+// -------------------- МАРШРУТИЗАЦИЯ ---------------------
 
-
-
+// Пользовательская страница /home
+//
 app.get('/home', function(req, res) {
   res.render('home');
 });
 
+// Пользовательская страница /about
+//
 app.get('/about', function(req, res) {
   res.render('about');
 });
 
-
 // Пользовательская страница 404
 // (промежуточное ПО)
+//
 app.use(function(req, res, next){
   res.status(404);
   res.render('404');
@@ -119,96 +138,17 @@ app.use(function(req, res, next){
 
 // Пользовательская страница 500
 // (промежуточное ПО)
+//
 app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500);
   res.render('500');
 });
 
+// Запускаем Express
+//
 app.listen(app.get('port'), function () {
   console.log('Express запущен на http://localhost:' + 
-              app.get('port') + '   для завершения нажмите Ctrl+C');
+              app.get('port') + 
+              '   для завершения нажмите Ctrl+C');
 });
-
-
-
-
-// 
-
-/*
-
-
-const http = require('http');
-const fs = require('fs');
-
-
-function serveStaticFile(res, path, contentType, responseCode) {
-  if (!responseCode) responseCode = 200;
-
-  console.log('filepath ==> ' + path);
-
-
-  fs.readFile(path, function (err, data) {
-    if (err) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('500 - Internal Error');
-    } else {
-      res.writeHead(responseCode, { 'Content-Type': contentType });
-      res.end(data);
-    }
-  });
-}
-
-
-const httpServer = http.createServer(function(req, res) {
-
-  console.log('');
-  console.log('request method ==> ' + req.method);
-  console.log('request url ==> ' + req.url);
-
-  // Приводим URL к единому виду, путём удаления
-  // строки запроса, необязательной косой черты
-  // в конце строки и приведения к нижнему регистру
-  const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
-  console.log('path ==> ' + path);
-
-  // Обрабатываем запрос
-
-  if (path == '') {
-    serveStaticFile(res, './public/index.html', 'text/html');
-
-  } else if (path == '/main.js') {
-    serveStaticFile(res, './public/main.js', 'application/javascript');
-  
-  } else if (path == '/main.css') {
-    serveStaticFile(res, './public/main.css', 'text/css');
-  
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not found');
-  }
-
-  */
-
-  /*
-  if (req.url == '/main.css') {
-    const css = fs.readFileSync('./public/main.css', 'utf8');
-    res.end(css);
-  } else if(req.url == '/main.js') {
-    const text = fs.readFileSync('./public/main.js', 'utf8');
-    res.end(text);
-  } else {
-    const text = fs.readFileSync('./public/index.html', 'utf8');
-    res.end(text);
-  }
-});
-*/
-
-/*
-console.log('port ==> ', process.env.PORT);
-
-httpServer.listen(process.env.PORT || 3000);
-
-console.log('Server started!');
-*/
-
